18f3b9ce117f9599e931cb91ba9e49e7690ba623
Anderson Alves <alves.hu@gmail.com>
2020-04-27 10:58:29 -0300

    first commit

---
+++ ExemploAnd/Filmes/Filmes/Controllers/FilmesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filmes.Models;
+
+namespace Filmes.Controllers
+{
+    public class FilmesController : Controller
+    {
+        // GET: Filmes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult detail()
+        {
+            var filme = new Models.Filme() { titulo = " Senhor dos aneis ", ano = 2000} ;
+            return View(filme);
+        }
+
+
+
+
+    }
+}
---
+++ ExemploAnd/Filmes/Filmes/Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Filmes.Models
+{
+
+    public string titulo { get; set; }
+    public int ano { get; set; }
+    public class Filme
+    {
+    }
+}
